1355bbd36970b81c385bce2cd682115948de1f81
jatinkohli <jatinpkohli@gmail.com>
2024-01-29 11:29:38 -0500

    Change test script for easier time changing which motor is used

--- backend/Testing/testRealRobotCommunication.py
+++ backend/Testing/testRealRobotCommunication.py
@@ -4,6 +4,8 @@ sys.path.append(".")
 from backend.KoalbyHumanoid.ArduinoSerial import ArduinoSerial
 import time
 
+motorID = 13
+
 s = ArduinoSerial()
 s.send_command("1")
 time.sleep(1)
@@ -12,11 +14,11 @@ time.sleep(1)
 
 startTime = time.time()
 for i in range(100):
-    s.send_command("5 5") # get position of motor 5 (right ankle)
+    s.send_command(f"5 {motorID}") # get position of motor 5 (right ankle)
     s.read_float()
 print(f"Time for read/write positions (average over 100): {(time.time() - startTime)/100}")
 
 time.sleep(10)
 while True:
-    s.send_command("5 5") # get position of motor 5 (right ankle)
+    s.send_command(f"5 {motorID}") # get position of motor 5 (right ankle)
     print(s.read_float())
