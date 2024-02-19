725c76b7bff73aca75bf3157ca05caa78c78b5af
Sbpena01 <sbpena@wpi.edu>
2024-02-19 00:10:57 -0500

    Assisted Walking - increased stability

--- backend/Testing/simAssistWalkTest.py
+++ backend/Testing/simAssistWalkTest.py
@@ -21,15 +21,18 @@ plotter = Plotter(10, False)
 robot.motors[1].target = (math.radians(80), 'P')
 robot.motors[6].target = (math.radians(-80), 'P')
 
-
 robot.motors[17].target = (math.radians(20), 'P')
 robot.motors[18].target = (math.radians(-40), 'P')
 robot.motors[19].target = (math.radians(-20), 'P')
 robot.motors[22].target = (math.radians(-20), 'P')
 robot.motors[23].target = (math.radians(40), 'P')
 robot.motors[24].target = (math.radians(20), 'P')
-robot.motors[3].target = (math.radians(90), 'P')
-robot.motors[8].target = (math.radians(-90), 'P')
+
+robot.motors[3].target = (math.radians(70), 'P')
+robot.motors[8].target = (math.radians(-70), 'P')
+robot.motors[0].target = (math.radians(20), 'P')
+robot.motors[5].target = (math.radians(-20), 'P')
+
 robot.motors[14].target = (0, 'P')
 
 prevTime = time.time()
@@ -40,21 +43,22 @@ while time.time() - simStartTime < 1:
     robot.IMUBalance(0,0)
     robot.moveAllToTarget()
 
+# [-0.0611,0.0575,-0.0054]
 
-
+## EVEN TO RIGHT FOOT FORW
 right_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
 right_angles = [[math.radians(20),math.radians(-40), math.radians(-20)], 
-                [math.radians(27.8416),math.radians(-54.0287), math.radians(-26.1871)], 
-                [math.radians(36.9495),math.radians(-56.3311), math.radians(-19.3816)], 
-                [math.radians(39.7712), math.radians(-48.2004), math.radians(-12.4292)], 
-                [math.radians(30.1578),math.radians(-32.3075), math.radians(-2.1477)]]
-right_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                [0.543488, -1.045386, -0.501898], 
+                [0.721075, -1.229439, -0.508364], 
+                [0.675096, -1.033531, -0.358434], 
+                [0.467840, -0.682081, -0.214241]]
+right_vels = [[0,0,0],[0.05, -0.1, -0.05], [0.05, 0.05, 0], [-0.05, 0.1, 0.05],[0,0,0]]
 right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
 left_setPointTimes = [[0,0,0],[8,8,8]]
 left_angles = [[math.radians(-20),math.radians(40), math.radians(20)],
-               [math.radians(-5.9552),math.radians(32.3075), math.radians(26.3523)]]
-left_vels = [[0,0,0],[0,0,0],[0,0,0]]
+               [-0.295607, 0.682080, 0.386473]]
+left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 left_accels = [[0,0,0],[0,0,0],[0,0,0]]
 
 right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
@@ -73,84 +77,58 @@ while True:
     robot.motors[22].target = (left_points[0], 'P')
     robot.motors[23].target = (left_points[1], 'P')
     robot.motors[24].target = (left_points[2], 'P')
-    robot.IMUBalance(0,0)
+    robot.IMUBalance(math.radians(15), 0)
     robot.moveAllToTarget()
     match(state):
         case 0: 
             if(time.time() - startTime >= 8):
                 ##ANGLES TO GO RIGHT TO LEFT
                 right_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                right_angles =  [[math.radians(30.1578),math.radians(-32.3075), math.radians(-2.1477)], 
-                                [math.radians(26.8052),math.radians(-39.0804), math.radians(-12.2751)], 
-                                [math.radians(20),math.radians(-40), math.radians(-20)], 
-                                [math.radians(10.1913), math.radians(-35.4586), math.radians(-25.2674)], 
-                                [math.radians(-3.8532),math.radians(-22.8189), math.radians(-25.6721)]]
-                right_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                right_angles =  [[0.467840, -0.682081, -0.214241], 
+                                [0.415031, -0.701593, -0.286562], 
+                                [0.349066, -0.698132, -0.349066], 
+                                [0.270305, -0.671400, -0.401095], 
+                                [0.177871, -0.618870, -0.440999]]
+                right_vels = [[0,0,0],[-0.1, 0, 0], [-0.1, 0.1, 0] ,[-0.1, 0, 0],[0,0,0]]
                 right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
                 left_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                left_angles = [[math.radians(-5.9552),math.radians(32.3075), math.radians(26.3525)], 
-                                [math.radians(-14.3033),math.radians(48.2004), math.radians(33.8971)], 
-                                [math.radians(-42.5117),math.radians(70.3060), math.radians(27.7943)], 
-                                [math.radians(-40.9558), math.radians(42.0062), math.radians(4.0504)], 
-                                [math.radians(-29.3238),math.radians(22.8186), math.radians(-4.5052)]]
-                left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                left_angles = [[-0.295607, 0.682080, 0.386473], 
+                                [-0.488609, 1.033534, 0.544925], 
+                                [-0.741970, 1.227072, 0.485102], 
+                                [-0.737818, 0.988358, 0.250540], 
+                                [-0.514302, 0.618868, 0.104566]]
+                left_vels = [[0,0,0],[-0.05, 0.1, 0.05],[-0.1, -0.1, 0],[0.05, -0.1, -0.05],[0,0,0]]
                 left_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
                 right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
                 left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
                 startTime = time.time()
-                print("State = 1")
+                print("Right To Left")
                 state = 1
             
         case 1: ##Right to Left
             if(time.time() - startTime >= 8):
                 ##ANGLES TO GO LEFT TO RIGHT
                 right_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                right_angles =  [[math.radians(-3.8532),math.radians(-22.8189), math.radians(-26.6721)], 
-                                [math.radians(6.0931),math.radians(-42.0062), math.radians(-35.9131)], 
-                                [math.radians(37.0296),math.radians(-70.3060), math.radians(-33.2765)], 
-                                [math.radians(39.7712), math.radians(-48.2004), math.radians(-8.4292)], 
-                                [math.radians(30.1578),math.radians(-32.3075), math.radians(-2.1497)]]
-                right_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                right_angles =  [[0.177871, -0.618870, -0.440999], 
+                                [0.373971, -0.988358, -0.614387], 
+                                [0.646288, -1.227072, -0.580784], 
+                                [0.675096, -1.033531, -0.358434], 
+                                [0.467840, -0.682081, -0.214241]]
+                right_vels = [[0,0,0],[0.05, -0.1, -0.05],[0.1, 0.1, 0],[-0.05, 0.1, 0.05],[0,0,0]]
                 right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
                 left_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                left_angles = [[math.radians(-29.3238),math.radians(22.8186), math.radians(-4.5052)], 
-                                [math.radians(-29.4673),math.radians(35.4585), math.radians(5.9912)], 
-                                [math.radians(-20),math.radians(40), math.radians(20)], 
-                                [math.radians(-16.9371), math.radians(39.0803), math.radians(22.1432)], 
-                                [math.radians(-5.9552),math.radians(32.3075), math.radians(26.3523)]]
+                left_angles = [[-0.514302, 0.618868, 0.104566], 
+                                [-0.481090, 0.671404, 0.190314], 
+                                [-0.432509, 0.698132, 0.265623], 
+                                [-0.370454, 0.701594, 0.331139], 
+                                [-0.295607, 0.682080, 0.386473]]
                 left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-                left_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                left_accels = [[0,0,0],[0,0,0],[0.1, -0.1, 0],[0,0,0],[0,0,0]]
                 right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
                 left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
-                print("State = 2")
+                print("Left To Right")
                 startTime = time.time()
                 state = 0
-        case 2: 
-            if(time.time() - startTime >= 8):
-                ##ANGLES TO GO FROM RIGHT TO LEFT
-                right_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                right_angles =  [[math.radians(30.1578),math.radians(-32.3075), math.radians(-2.1477)], 
-                                [math.radians(26.8052),math.radians(-39.0804), math.radians(-12.2751)], 
-                                [math.radians(20),math.radians(-40), math.radians(-20)], 
-                                [math.radians(10.1913), math.radians(-35.4586), math.radians(-25.2674)], 
-                                [math.radians(-3.8532),math.radians(-22.8189), math.radians(-25.6721)]]
-                right_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-                right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-
-                left_setPointTimes = [[0,0,0], [2,2,2], [4,4,4], [6,6,6], [8,8,8]]
-                left_angles = [[math.radians(-5.9552),math.radians(32.3075), math.radians(26.3525)], 
-                                [math.radians(-14.3033),math.radians(48.2004), math.radians(33.8971)], 
-                                [math.radians(-42.5117),math.radians(70.3060), math.radians(27.7943)], 
-                                [math.radians(-40.9558), math.radians(42.0062), math.radians(4.0504)], 
-                                [math.radians(-29.3238),math.radians(22.8186), math.radians(-4.5052)]]
-                left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-                left_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-
-                right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
-                left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
-                startTime = time.time()
-                print("State = 1")
-                state = 1
