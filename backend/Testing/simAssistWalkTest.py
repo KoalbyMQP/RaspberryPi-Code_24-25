ffadc466202932a91c090d5be6f8a25c376ca181
Sbpena01 <sbpena@wpi.edu>
2024-03-24 23:46:44 -0400

    Updated simAssistWalkTest.py

--- backend/Testing/simAssistWalkTest.py
+++ backend/Testing/simAssistWalkTest.py
@@ -3,11 +3,8 @@ import sys, time, math
 import numpy as np 
 sys.path.append("./")
 from backend.KoalbyHumanoid.Robot import Robot
-from backend.KoalbyHumanoid.trajPlannerPose import TrajPlannerPose
 from backend.KoalbyHumanoid.trajPlannerTime import TrajPlannerTime
-from backend.KoalbyHumanoid.Config import Joints
-import matplotlib.pyplot as plt
-from backend.KoalbyHumanoid.Plotter import Plotter
+from backend.Testing import assistWalkViaPoints as via
 
 # Edit to declare if you are testing the sim or the real robot
 is_real = False
@@ -16,6 +13,7 @@ robot = Robot(is_real)
 
 print("Setup Complete")
 
+#Starting Agnles
 robot.motors[0].target = (math.radians(-20), 'P')
 robot.motors[1].target = (math.radians(90), 'P')
 robot.motors[3].target = (math.radians(110), 'P')
@@ -35,68 +33,75 @@ robot.motors[24].target = (math.radians(20), 'P')
 prevTime = time.time()
 simStartTime = time.time()
 
-while time.time() - simStartTime < 8:
+while time.time() - simStartTime < 2:
     time.sleep(0.01)
     robot.IMUBalance(0,0)
     robot.moveAllToTarget()
 
 ## EVEN TO RIGHT FOOT FORWARD
-right_setPointTimes = [[0,0,0], [1,1,1], [2,2,2], [3,3,3], [4,4,4]]
-right_angles = [[math.radians(20),math.radians(-40), math.radians(-20)], 
-                [0.543488, -1.045386, -0.501898], 
-                [0.721075, -1.229439, -0.508364], 
-                [0.675096, -1.033531, -0.358434], 
-                [0.467840, -0.682081, -0.214241]]
-right_vels = [[0,0,0],[0.05, -0.1, -0.05], [0.05, 0.05, 0], [-0.05, 0.1, 0.05],[0,0,0]]
-right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+rLeg_tj = TrajPlannerTime(via.rf_Even2Right[0], via.rf_Even2Right[1], via.rf_Even2Right[2], via.rf_Even2Right[3])
+lLeg_tj = TrajPlannerTime(via.lf_Even2Right[0], via.lf_Even2Right[1], via.lf_Even2Right[2], via.lf_Even2Right[3])
+rArm_tj = TrajPlannerTime(via.ra_grabCart[0], via.ra_grabCart[1], via.ra_grabCart[2], via.ra_grabCart[3])
+lArm_tj = TrajPlannerTime(via.la_grabCart[0], via.la_grabCart[1], via.la_grabCart[2], via.la_grabCart[3])
 
-left_setPointTimes = [[0,0,0],[4,4,4]]
-left_angles = [[math.radians(-20),math.radians(40), math.radians(20)],
-               [-0.295607, 0.682080, 0.386473]]
-left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-left_accels = [[0,0,0],[0,0,0],[0,0,0]]
-
-right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
-left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
 state = 0
 startTime = time.time()
-lastTime = startTime
+
 print("State = 0")
+
+## Grabbing cart
+while time.time() - startTime < 3:
+    r_points = rArm_tj.getQuinticPositions(time.time() - startTime)
+    l_points = lArm_tj.getQuinticPositions(time.time() - startTime)
+
+    robot.motors[0].target = (r_points[0], 'P')
+    robot.motors[1].target = (r_points[1], 'P')
+    robot.motors[2].target = (r_points[2], 'P')
+    robot.motors[3].target = (r_points[3], 'P')
+    robot.motors[4].target = (r_points[4], 'P')
+    
+    robot.motors[5].target = (l_points[0], 'P')
+    robot.motors[6].target = (l_points[1], 'P')
+    robot.motors[7].target = (l_points[2], 'P')
+    robot.motors[8].target = (l_points[3], 'P')
+    robot.motors[9].target = (l_points[4], 'P')
+
+    robot.IMUBalance(0, 0)
+    robot.moveAllToTarget()
+
+
+startTime = time.time()
+test = TrajPlannerTime(via.lf_test[0], via.lf_test[1], via.lf_test[2], via.lf_test[3])
+while time.time() - startTime < 4:
+    testp = test.getQuinticPositions(time.time() - startTime)
+    robot.motors[20].target = (testp[0], 'P')
+    robot.motors[21].target = (testp[1], 'P')
+    robot.motors[22].target = (testp[2], 'P')
+    robot.motors[23].target = (testp[3], 'P')
+    robot.motors[24].target = (testp[4], 'P')
+    robot.IMUBalance(0, 0)
+    robot.moveAllToTarget()
+
+time.sleep(10)
+##  Walking
+startTime = time.time()
 while True:
-    right_points = right_tj.getQuinticPositions(time.time() - startTime)
-    left_points = left_tj.getQuinticPositions(time.time() - startTime)
+    right_points = rLeg_tj.getQuinticPositions(time.time() - startTime)
+    left_points = lLeg_tj.getQuinticPositions(time.time() - startTime)
     robot.motors[17].target = (right_points[0], 'P')
     robot.motors[18].target = (right_points[1], 'P')
     robot.motors[19].target = (right_points[2], 'P')
     robot.motors[22].target = (left_points[0], 'P')
     robot.motors[23].target = (left_points[1], 'P')
     robot.motors[24].target = (left_points[2], 'P')
-    robot.IMUBalance(0, 0)
+    robot.IMUBalance(math.radians(-20), 0)
     robot.moveAllToTarget()
     match(state):
         case 0: 
             if(time.time() - startTime >= 4):
                 ##ANGLES TO GO RIGHT TO LEFT
-                right_setPointTimes = [[0,0,0], [1,1,1], [2,2,2], [3,3,3], [4,4,4]]
-                right_angles =  [[0.467840, -0.682081, -0.214241], 
-                                [0.415031, -0.701593, -0.286562], 
-                                [0.349066, -0.698132, -0.349066], 
-                                [0.270305, -0.671400, -0.401095], 
-                                [0.177871, -0.618870, -0.440999]]
-                right_vels = [[0,0,0],[-0.1, 0, 0], [-0.1, 0.1, 0] ,[-0.1, 0, 0],[0,0,0]]
-                right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-
-                left_setPointTimes = [[0,0,0], [1,1,1], [2,2,2], [3,3,3], [4,4,4]]
-                left_angles = [[-0.295607, 0.682080, 0.386473], 
-                                [-0.488609, 1.033534, 0.544925], 
-                                [-0.741970, 1.227072, 0.485102], 
-                                [-0.737818, 0.988358, 0.250540], 
-                                [-0.514302, 0.618868, 0.104566]]
-                left_vels = [[0,0,0],[-0.05, 0.1, 0.05],[-0.1, -0.1, 0],[0.05, -0.1, -0.05],[0,0,0]]
-                left_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-
-                right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
-                left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
+                rLeg_tj = TrajPlannerTime(via.rf_Right2Left[0], via.rf_Right2Left[1], via.rf_Right2Left[2], via.rf_Right2Left[3])
+                lLeg_tj = TrajPlannerTime(via.lf_Right2Left[0], via.lf_Right2Left[1], via.lf_Right2Left[2], via.lf_Right2Left[3])
                 startTime = time.time()
                 print("Right To Left")
                 state = 1
@@ -104,25 +109,8 @@ while True:
         case 1: ##Right to Left
             if(time.time() - startTime >= 4):
                 ##ANGLES TO GO LEFT TO RIGHT
-                right_setPointTimes = [[0,0,0], [1,1,1], [2,2,2], [3,3,3], [4,4,4]]
-                right_angles =  [[0.177871, -0.618870, -0.440999], 
-                                [0.373971, -0.988358, -0.614387], 
-                                [0.646288, -1.227072, -0.580784], 
-                                [0.675096, -1.033531, -0.358434], 
-                                [0.467840, -0.682081, -0.214241]]
-                right_vels = [[0,0,0],[0.05, -0.1, -0.05],[0.1, 0.1, 0],[-0.05, 0.1, 0.05],[0,0,0]]
-                right_accels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-
-                left_setPointTimes = [[0,0,0], [1,1,1], [2,2,2], [3,3,3], [4,4,4]]
-                left_angles = [[-0.514302, 0.618868, 0.104566], 
-                                [-0.481090, 0.671404, 0.190314], 
-                                [-0.432509, 0.698132, 0.265623], 
-                                [-0.370454, 0.701594, 0.331139], 
-                                [-0.295607, 0.682080, 0.386473]]
-                left_vels = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-                left_accels = [[0,0,0],[0,0,0],[0.1, -0.1, 0],[0,0,0],[0,0,0]]
-                right_tj = TrajPlannerTime(right_setPointTimes, right_angles, right_vels, right_accels)
-                left_tj = TrajPlannerTime(left_setPointTimes, left_angles, left_vels, left_accels)
+                rLeg_tj = TrajPlannerTime(via.rf_Left2Right[0], via.rf_Left2Right[1], via.rf_Left2Right[2], via.rf_Left2Right[3])
+                lLeg_tj = TrajPlannerTime(via.lf_Left2Right[0], via.lf_Left2Right[1], via.lf_Left2Right[2], via.lf_Left2Right[3])
                 print("Left To Right")
                 startTime = time.time()
                 state = 0
