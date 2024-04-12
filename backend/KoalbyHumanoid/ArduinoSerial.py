586c643a998d13a3aa298d1353901b7ecf2de419
sfanning5 <stephen.e.fanning@gmail.com>
2024-04-12 15:42:00 -0400

    Allowed RasPi code to work on window + Turns off EM when stopping code

--- backend/KoalbyHumanoid/ArduinoSerial.py
+++ backend/KoalbyHumanoid/ArduinoSerial.py
@@ -7,16 +7,24 @@ class ArduinoSerial(object):
     def __init__(self):
         # to run connected to Arduino
         ports = list_ports.comports() # Auto detects COM port
-        self.ser = serial.Serial(ports[0].device, 115200, timeout=1)
+        if(len(ports) > 0):
+            self.ser = serial.Serial(ports[0].device, 115200, timeout=1)
 
-        self.ser.reset_input_buffer()
-        time.sleep(3)
+            self.ser.reset_input_buffer()
+            time.sleep(3)
+        else:
+            print("No serial ports found, continuing without serial ports.")
+            self.ser = None
 
     def send_command(self, command):  # sends a command to the arduino from the RasPi
-        message = str.encode(command + "\n") # Firmware looks for '\n' as command terminator
-        self.ser.write(message)
+        if(self.ser):
+            message = str.encode(command + "\n") # Firmware looks for '\n' as command terminator
+            self.ser.write(message)
 
     def read_float(self): # reads a float message from the arduino (all messages are floats)
-        line = self.ser.readline()
-        line = line.decode('utf-8').strip()
-        return line
+        if(self.ser):
+            line = self.ser.readline()
+            line = line.decode('utf-8').strip()
+            return line
+        else:
+            return ""
