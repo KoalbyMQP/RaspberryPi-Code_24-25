c90f15e2eda416555c5ddfdebe9c645a1ffb7370
Lily Jones <127898963+Lily7jones@users.noreply.github.com>
2024-10-24 14:35:58 -0400

    added states and inputs

--- backend/Testing/mpcModel.py
+++ backend/Testing/mpcModel.py
@@ -22,6 +22,7 @@ z_c = robot.CoM[3] #Height of COM of the Robot
 
 
 #setting up the model
+
 #creating state space model 
 a = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]])
 b = np.array([[0], [0], [1]])
@@ -30,6 +31,21 @@ d = np.array([[0], [0], [0]])
 
 lip_x = signal.StateSpace(a, b, c, d)
 lip_y = signal.StateSpace(a, b, c, d)
+
+#setting up states
+# x = model.set_variable(var_type='_x', var_name='x', shape=(1,1))
+# xd = model.set_variable(var_type='_x', var_name='xd', shape=(1,1))
+# xdd = model.set_variable(var_type='_x', var_name='xdd', shape=(1,1))
+lip_x = model.set_variable(var_type='_x', var_name='lip_x', shape=(3, 1))
+# y = model.set_variable(var_type='_x', var_name='y', shape=(1,1))
+# yd = model.set_variable(var_type='_x', var_name='yd', shape=(1,1))
+# ydd = model.set_variable(var_type='_x', var_name='ydd', shape=(1,1))
+lip_y = model.set_variable(var_type='_x', var_name='lip_y', shape=(3,1))
+
+#inputs 
+xddd = model.set_variable(var_type='_u', var_name='xddd', shape=(1,1))
+yddd = model.set_variable(var_type= '_u', var_name='yddd', shape=(1,1))
+
 
 
 
