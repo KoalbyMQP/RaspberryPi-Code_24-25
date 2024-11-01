e6e1266dd1b68085cf393826de600b969538910e
Lily Jones <127898963+Lily7jones@users.noreply.github.com>
2024-11-01 11:39:01 -0400

    updates and edits

--- backend/Testing/mpcModel.py
+++ backend/Testing/mpcModel.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy as scipy
 from scipy import signal
-import backend.KoalbyHumanoid.Robot as Robot
+from casadi import SX
+#import backend.KoalbyHumanoid.Robot as Robot
 
 # Add do_mpc to path. This is not necessary if it was installed via pip.
 import sys
@@ -12,14 +13,18 @@ sys.path.append(rel_do_mpc_path)
 # Import do_mpc package:
 import do_mpc
 
-robot = Robot
+#robot = Robot
 model_type = 'continuous' # either 'discrete' or 'continuous'
 model = do_mpc.model.Model(model_type)
 
 #system constants 
 g = 9.8 #gravity 
-z_c = robot.CoM[3] #Height of COM of the Robot
-
+z_c = 10#robot.CoM[3] #Height of COM of the Robot
+ts = .5 # time step 
+x = 10 #this is the position of the xCOM
+px = 5 #I think this is the wanted position of xCOM
+y = 10 #position of yCOM
+py = 5 #wanted position of yCOM
 
 #setting up the model
 
@@ -33,18 +38,23 @@ lip_x = signal.StateSpace(a, b, c, d)
 lip_y = signal.StateSpace(a, b, c, d)
 
 #setting up states
-# x = model.set_variable(var_type='_x', var_name='x', shape=(1,1))
-# xd = model.set_variable(var_type='_x', var_name='xd', shape=(1,1))
-# xdd = model.set_variable(var_type='_x', var_name='xdd', shape=(1,1))
-lip_x = model.set_variable(var_type='_x', var_name='lip_x', shape=(3, 1))
-# y = model.set_variable(var_type='_x', var_name='y', shape=(1,1))
-# yd = model.set_variable(var_type='_x', var_name='yd', shape=(1,1))
-# ydd = model.set_variable(var_type='_x', var_name='ydd', shape=(1,1))
-lip_y = model.set_variable(var_type='_x', var_name='lip_y', shape=(3,1))
+#x = model.set_variable(var_type='_x', var_name='x', shape=(1,1))
+#xd = model.set_variable(var_type='_x', var_name='xd', shape=(1,1))
+xdd = model.set_variable(var_type='_x', var_name='xdd', shape=(1,1))
+
+#y = model.set_variable(var_type='_x', var_name='y', shape=(1,1))
+#yd = model.set_variable(var_type='_x', var_name='yd', shape=(1,1))
+ydd = model.set_variable(var_type='_x', var_name='ydd', shape=(1,1))
 
 #inputs 
 xddd = model.set_variable(var_type='_u', var_name='xddd', shape=(1,1))
 yddd = model.set_variable(var_type= '_u', var_name='yddd', shape=(1,1))
+
+A = np.array([[1, ts, ts**2/2], [0, 1, ts], [0, 0, 1]])
+B = np.array([[ts**3/6], [ts**2/2], [ts]])
+C = np.array([1, 0, -z_c/g])
+
+model.setup()
 
 
 
