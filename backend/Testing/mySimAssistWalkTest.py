adb68e6ebc58eec5ec3e1f47a92e34c354099f4b
Sahil-Mirani <153775261+Expired-Cheese@users.noreply.github.com>
2024-11-02 19:33:00 -0400

    Much better now, we need to fix the orientation so the trajectories make sense

--- backend/Testing/mySimAssistWalkTest.py
+++ backend/Testing/mySimAssistWalkTest.py
@@ -17,7 +17,7 @@ def setup_robot(is_real):
     print("Setup Complete")
     return robot
 
-def update_trajectory_and_control(robot, n_points, right_points, left_points, start_time):
+def update_trajectory_and_control(robot, right_points, left_points, start_time):
     currentTime = time.time() - start_time
 
     # Right Leg
@@ -49,86 +49,76 @@ def main():
         "backend/Testing/robotChain.urdf",
         base_elements=['RightHip', 'RightLegRotator']
     )
+    
+    x_base = 0.04
+    y_base = -0.385
+    z_base = 0.0
 
-    y_avg_position = -0.385
-    z_avg_position = 0.0
-    y_scale = 2
-    z_scale = 1
-    # target_positions_left = np.array([
-    #     [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.0],
-    #     [0.04,  -0.285 , -0.05],
-    #     [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.1],
-    #     [0.04,  -0.385 , -0.1]
-    # ])
-    # target_positions_right = np.array([
-    #     [0.04,  y_avg_position + y_scale*0.050 , z_avg_position + z_scale*0.1],
-    #     [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.1],
-    #     [0.04,  y_avg_position + y_scale*0.100 , z_avg_position + z_scale*0.2],
-    #     [0.04,  y_avg_position + y_scale*0.050 , z_avg_position + z_scale*0.1]
-    # ])
+    left_base = [x_base, y_base, z_base]
+    left_back_flat = [x_base, y_base + 0.085, z_base - 0.241]
+    left_back_not_flat = [x_base, y_base, z_base - 0.241]
+    left_center_top = [x_base, y_base + 0.1, z_base]
+    left_forward_top = [x_base, y_base + 0.1, z_base + 0.2]
+    left_almost_center_top = [x_base, y_base + 0.085, z_base]
 
-    target_positions_left = np.array([
-        [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.0],
-        [0.04,  -0.285 , -0.05],
-        [0.04,  -0.385 , -0.1],
-        [0.04,  -0.385 , -0.2]
-    ])
-    target_positions_right = np.array([
-        [0.04,  y_avg_position + y_scale*0.050 , z_avg_position + z_scale*0.1],
-        [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.1],
-        [0.04,  y_avg_position + y_scale*0.100 , z_avg_position + z_scale*0.2],
-        [0.04,  y_avg_position + y_scale*0.050 , z_avg_position + z_scale*0.1]
-    ])
+    right_base = [x_base, y_base, z_base]
+    right_back_flat = [x_base, y_base + 0.085, z_base - 0.241]
+    right_back_not_flat = [x_base, y_base, z_base - 0.241]
+    right_center_top = [x_base, y_base + 0.1, z_base]
+    right_forward_top = [x_base, y_base + 0.1, z_base + 0.2]
+    right_almost_center_top = [x_base, y_base + 0.085, z_base]
 
-    left_base = [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.0]
-    left_center_top = [0.04, y_avg_position + y_scale * 0.05, z_avg_position + z_scale*0.1]
-    left_forward_top = [0.04,  y_avg_position + y_scale*0.1 , z_avg_position + z_scale*0.2]
-    right_back_flat = [0.04,  y_avg_position + y_scale*0.1 , z_avg_position + z_scale*0.2] #Doesn't work, the leg peaks, if z goes below 0
+    target_orientation_left = [0, 1, 0]
+    target_orientation_right = [0, 1, 0]
 
-    right_base = [0.04,  y_avg_position + y_scale*0.050 , z_avg_position + z_scale*0.1]
-    right_center_top = [0.04,  y_avg_position + y_scale*0.000 , z_avg_position + z_scale*0.1]
-    right_forward_top = [0.04,  y_avg_position + y_scale*0.1 , z_avg_position + z_scale*0.2]
-    # left_back_flat -> Wouldn't work cause leg peaks, if z goes negative
-
-
-    trajectory_left = []
-    trajectory_right = []
-
-    trajectory_left = TrajPlannerTime([[0,0,0], [5,5,5], [10,10,10], [15, 15,15]], target_positions_left, [[0,0,0],[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
-    trajectory_right = TrajPlannerTime([[0,0,0], [5,5,5], [10,10,10], [15,15,15]], target_positions_right, [[0,0,0],[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
-
-    n_points = 100
-    t_left = np.linspace(0, 1, len(target_positions_left))
-    splines_left = [CubicSpline(t_left, target_positions_left[:, dim]) for dim in range(3)]
-    # trajectory_left = np.vstack([spline(np.linspace(0, 1, n_points)) for spline in splines_left]).T
-
-    t_right = np.linspace(0, 1, len(target_positions_right))
-    splines_right = [CubicSpline(t_right, target_positions_right[:, dim]) for dim in range(3)]
-    # trajectory_right = np.vstack([spline(np.linspace(0, 1, n_points)) for spline in splines_right]).T
-
-    left_points = []
-    right_points = []
-
-    target_orientation = [0, 0, 0.05]
-
-    state = 0
     initial_position_left = [0, 0, 0, 0, 0]
+    initial_position_right = [0, 0, 0, 0, 0]
 
-    initial_position_right = [-0.8818984203844015, 2.090247231413886, -7.376601873229693e-06, 0, 0]
+    all_trajectories = []
 
+    #RLF = Right Leg First 
+    #LLF = Left Leg First
+    
+    #RLP = Right Leg Positions
+    #LLP = Left Leg Positions
+    RLF_RLP = [right_base, right_center_top, right_forward_top, right_almost_center_top, right_base, right_base, right_base]
+    RLF_LLP = [left_base, left_base, left_base, left_back_flat, left_back_not_flat, left_center_top, left_base]
 
+    LLF_RLP = [right_base, right_base, right_base, right_back_flat, right_back_not_flat, right_center_top, right_base]
+    LLF_LLP = [left_base, left_center_top, left_forward_top, left_almost_center_top, left_base, left_base, left_base]
+
+    total_time = []
+    total_vel = []
+    total_acc = []
+    step_time = 2.5
+    for i in range(len(RLF_LLP)):
+        total_time.append([i * step_time, i * step_time, i * step_time])
+        total_vel.append([0, 0, 0])
+        total_acc.append([0, 0, 0])
+
+    print(total_time, RLF_LLP, total_vel, total_acc)
+
+    RLF_LLP_traj = TrajPlannerTime(total_time, RLF_LLP, total_vel, total_acc)
+    RLF_RLP_traj = TrajPlannerTime(total_time, RLF_RLP, total_vel, total_acc)
+    all_trajectories.append([RLF_LLP_traj,RLF_RLP_traj])
+
+    LLF_LLP_traj = TrajPlannerTime(total_time, LLF_LLP, total_vel, total_acc)
+    LLF_RLP_traj = TrajPlannerTime(total_time, LLF_RLP, total_vel, total_acc)
+    all_trajectories.append([LLF_LLP_traj,LLF_RLP_traj])
+
+    traj_number=0
     while True:
         start_time = time.time()
-        count = 1
-        while time.time() - start_time < 15:
-                left_quintic = trajectory_left.getQuinticPositions(time.time() - start_time)
-                right_quintic = trajectory_right.getQuinticPositions(time.time() - start_time)
-            
+        while time.time() - start_time < step_time*(len(total_time)-1):
+                left_quintic = all_trajectories[traj_number][0].getQuinticPositions(time.time() - start_time)
+                right_quintic = all_trajectories[traj_number][1].getQuinticPositions(time.time() - start_time)
+                print(left_quintic)
+
                 target_position_left = left_quintic
                 ik_solution_left = left_leg_chain.inverse_kinematics(
                     target_position_left,
                     initial_position=initial_position_left,
-                    target_orientation=target_orientation,
+                    target_orientation=target_orientation_left,
                     orientation_mode='Z' 
                 )
                 initial_position_left = ik_solution_left
@@ -138,20 +128,21 @@ def main():
                 ik_solution_right = right_leg_chain.inverse_kinematics(
                     target_position_right,
                     initial_position=initial_position_right,
-                    target_orientation=target_orientation,
+                    target_orientation=target_orientation_right,
                     orientation_mode='Z'
                 )
                 initial_position_right = ik_solution_right
                 # print(left_quintic)
-                print(ik_solution_left)
+                # print(ik_solution_left)
                 
-                update_trajectory_and_control(robot, n_points, ik_solution_right, ik_solution_left, start_time)
-
-                count += 1
-
-            # if time.time() - start_time >= 3:
-            #     state = (state + 1) % 2  
-            #     start_time = time.time()
+                update_trajectory_and_control(robot, ik_solution_right, ik_solution_left, start_time)
+        if(traj_number < (len(all_trajectories)-1)):
+            print("Hello")
+            traj_number = traj_number + 1
+        else:
+            print("Hi")
+            traj_number = 0
+        print(traj_number)
 
 if __name__ == "__main__":
     main()
