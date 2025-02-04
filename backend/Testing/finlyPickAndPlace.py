8e17aafa2750b4a85a361e365a4a47ccc59f07c7
elowyn2003 <elowyn2003@gmail.com>
2025-02-03 20:25:55 -0500

    most recent chnage to head

--- backend/Testing/finlyPickAndPlace.py
+++ backend/Testing/finlyPickAndPlace.py
@@ -20,20 +20,27 @@ from backend.Testing import finlyViaPoints as via
 
 
 
-
-left_leg_chain = Chain.from_urdf_file(
-    "backend/Testing/FullAssemFin.urdf",
-    base_elements=['left_shoulder1', 'left_shoulder_twist']
+# Creating URDF chain for left arm 
+left_arm_chain = Chain.from_urdf_file(
+    "backend/Testing/FinleyJNEWARMS_2024_straight_4.urdf",
+    base_elements=['shoulder1_left', 'shoulder1_left'],
+    active_links_mask=[False, True, True, True, True, True, True]  
 )
+
+# creating URDF chain for camera
+camera = Chain.from_urdf_file(
+    "backend/Testing/\FinleyJNEWARMS_2024_straight_4.urdf",
+    base_elements=['neck', 'neck']   
+)
+
+#forward kinematics for camera chain
+camera_angles=np.array([0,0,0,0])
+camera_frame_transformation=camera.forward_kinematics(camera_angles)
 
 
 # Edit to declare if you are testing the sim or the real robot
 is_real = False
-
-
 robot = Robot(is_real)
-
-
 print("Setup Complete")
 
 
@@ -43,12 +50,14 @@ print("Setup Complete")
 final_position=np.array([0.07089,  -0.26614,  .68187])
 
 
-motor_angle_joint=np.array([0,0,0,0,0])
-
 
 #Starting Agnles
 
-robot.motors[5].target = (math.radians(90), 'P')
+# robot.motors[25].target = (math.radians(30), 'P')
+# robot.motors[26].target = (math.radians(10), 'P')
+
+
+robot.motors[5].target = (math.radians(0), 'P')
 robot.motors[6].target = (math.radians(90), 'P')
 robot.motors[7].target = (math.radians(0), 'P')
 robot.motors[8].target = (math.radians(0), 'P')
@@ -58,65 +67,61 @@ robot.motors[10].target = (math.radians(0), 'P')
 
 
 ik_solution_2=np.array([0,0,0,0,0,0,0])
+
+
+# centering all angles to zero
 prevTime = time.time()
-
-
 simStartTime = time.time()
-
-
 while time.time() - simStartTime < 2:
     time.sleep(0.01)
     robot.IMUBalance(0,0)
     robot.moveAllToTarget()
 
 
+# conversion of final points from camera coordinate systm to rorbot coordinate system 
+final_points=np.array([0, .3, 0])
+B=np.array([[final_points[0]],[final_points[1]],[final_points[2]],[1]])
+A= camera_frame_transformation
+final_points=np.array([0,0, 0])
+C = np.dot(A, B)
+
+
+
 
 leftArmTraj = [
     [[0,0,0], [20,20,20]],
-    [[.49513,  -.04901, .28036],
-     [.12938,  -.03073, .60344]],
+    [[.49076,  -.08197, .76541],
+   [C[0],  C[1], C[2]] ],
     [[0,0,0], [0,0,0]],
     [[0,0,0], [0,0,0]]
 ]
 
-# get=np.array([.41263 +.02042,  -.01513 +.02647, .26667 -.02361])
-# print(get)
-final_position=left_leg_chain.forward_kinematics(ik_solution_2)
-print(final_position)
+final_position=left_arm_chain.forward_kinematics(ik_solution_2)
 
-# arred=np.array([ -3.29889867e-14,  1.33762582e+00 , 5.24790953e-01 ,-2.16606497e-02, 1.35522250e-02, -2.52929247e+00 , 0.00000000e+00])
-# help=left_leg_chain.forward_kinematics(arred)
-# print(help)
-
-## EVEN TO RIGHT FOOT FORWARD
-# rArm_tj = TrajPlannerTime(via.ra_grabCart[0], via.ra_grabCart[1], via.ra_grabCart[2], via.ra_grabCart[3])
 lArm_tj_joint = TrajPlannerTime(leftArmTraj[0], leftArmTraj[1], leftArmTraj[2], leftArmTraj[3])
 
 
-state = 0
 
 
 startTime = time.time()
 
-
-print("phase 2")
-
-while time.time() - startTime < 20:
-     
-        target_position_task = lArm_tj_joint.getQuinticPositions(time.time() - startTime)
-        target_position_2 = np.array([(target_position_task[0]), (target_position_task[1]), (target_position_task[2])])
-        ik_solution = left_leg_chain.inverse_kinematics(target_position_2, initial_position=ik_solution_2 )
-        ik_solution_2=ik_solution
-        motor_angle_task=ik_solution
-
-        robot.motors[5].target = (motor_angle_task[1], 'P')
-        robot.motors[6].target = (motor_angle_task[2], 'P')
-        robot.motors[7].target = (motor_angle_task[3], 'P')
-        robot.motors[8].target = (motor_angle_task[4], 'P')
-        robot.motors[9].target = (motor_angle_task[5], 'P')
+# while time.time() - startTime < 20:
         
-        print(motor_angle_task)
+     
+#         target_position_task = lArm_tj_joint.getQuinticPositions(time.time() - startTime)
+#         target_position_2 = np.array([(target_position_task[0]), (target_position_task[1]), (target_position_task[2])])
+#         ik_solution = left_arm_chain.inverse_kinematics(target_position_2, initial_position=ik_solution_2 )
+#         ik_solution_2=ik_solution
+#         motor_angle_task=ik_solution
 
-        robot.IMUBalance(0, 0)
-        robot.moveAllToTarget()
+#         robot.motors[5].target = (motor_angle_task[1], 'P')
+#         robot.motors[6].target = (motor_angle_task[2], 'P')
+#         robot.motors[7].target = (motor_angle_task[3], 'P')
+#         robot.motors[8].target = (motor_angle_task[4], 'P')
+#         robot.motors[9].target = (motor_angle_task[5], 'P')
+        
+#         print(motor_angle_task)
+
+#         robot.IMUBalance(0, 0)
+#         robot.moveAllToTarget()
  
--- backend/Testing/testIKPY.py
+++ backend/Testing/testIKPY.py
@@ -1,24 +1,27 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from ikpy.chain import Chain
 from ikpy.utils import plot as plot_utils
 
 left_leg_chain = Chain.from_urdf_file(
-        "backend/Testing/FullAssemFin.urdf",
-    base_elements=['left_shoulder1', 'left_shoulder_twist']
+    "backend/Testing/FinleyJNEWARMS_2024_2.urdf",
+    base_elements=['shoulder1_left', 'shoulder1_left'],
+    active_links_mask=[False, True, True, True, True, True, True]
+    
 )
 
-target_positions = np.array([0.1, -0.01, -0.45])
+target_positions = np.array([.39076,  -.08197, .76541])
 
 ik_solution = left_leg_chain.inverse_kinematics(target_positions)
 ik_solution_2=np.array([0,0,0,0,0,0,0])
 print("Joint angles:", ik_solution)
 
-real_frame = left_leg_chain.forward_kinematics(ik_solution)
-
+real_frame = left_leg_chain.forward_kinematics(ik_solution_2)
+#print("Joint angles:", real_frame)
 fig, ax = plot_utils.init_3d_figure()
 
-left_leg_chain.plot(ik_solution_2, ax)
+left_leg_chain.plot(ik_solution, ax)
 
 ax.set_xlim([-1, 1])
 ax.set_ylim([-1, 1])
