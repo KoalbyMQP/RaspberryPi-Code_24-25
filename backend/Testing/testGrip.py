87654f2edd65fa78808b41ad253bb448df7607eb
David Alex <dgalex@wpi.edu>
2025-02-26 13:37:40 -0500

    edits

--- backend/Testing/testGrip.py
+++ backend/Testing/testGrip.py
@@ -1,16 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
-
 from ikpy.chain import Chain
 from ikpy.utils import plot as plot_utils
-
-
 import sys, time, math, array
-
-
 import numpy as np
 sys.path.append("./")
 from backend.KoalbyHumanoid.Robot import Robot
@@ -22,11 +14,5 @@ robot = Robot(is_real)
 print("Setup Complete")
 robot.motors[27].target = (math.radians(60), 'P')
 print("targeted")
-prevTime = time.time()
-simStartTime = time.time()
-while time.time() - simStartTime < 2:
-    time.sleep(0.01)
-    #robot.IMUBalance(0,0)
-    robot.moveAllToTarget()
-
+robot.moveAllToTarget()
 print("closed")
